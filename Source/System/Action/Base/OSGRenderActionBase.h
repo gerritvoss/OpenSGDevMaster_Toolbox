55a01a943027e1ca09d4f57b8d5c9de7239bbe89
David Kabala <djkabala@gmail.com>
2010-09-11 22:54:16 -0500

    Added: Member variable, _bManageStatReset, to RenderActionBase to control whether or not RenderActionBase calls reset on an attached StatCollector.  If _bManageStatReset is true then the StatCollector is reset at the start of a traversal, but is not reset if false.

--- Source/System/Action/Base/OSGRenderActionBase.h
+++ Source/System/Action/Base/OSGRenderActionBase.h
@@ -131,31 +131,31 @@ class OSG_SYSTEM_DLLMAPPING RenderActionBase : public RenderActionBaseParent
     /*! \name               Rendering Environment                          */
     /*! \{                                                                 */
 
-          Viewport      *getViewport      (void                      ) const;
-          void           setViewport      (Viewport      *pViewport  );
+          Viewport      *getViewport       (void                      ) const;
+          void           setViewport       (Viewport      *pViewport  );
     
-          Camera        *getCamera        (void                      ) const;
-          void           setCamera        (Camera        *pCamera    );
+          Camera        *getCamera         (void                      ) const;
+          void           setCamera         (Camera        *pCamera    );
 
-          Background    *getBackground    (void                      ) const;
-          void           setBackground    (Background    *pBackground);
+          Background    *getBackground     (void                      ) const;
+          void           setBackground     (Background    *pBackground);
     
-          Window        *getWindow        (void                      ) const;
-          void           setWindow        (Window        *pWindow    );
+          Window        *getWindow         (void                      ) const;
+          void           setWindow         (Window        *pWindow    );
     
-          Material      *getGlobalOverride(void                      ) const;
-          void           setGlobalOverride(Material      *pMat       );
+          StatCollector *getStatCollector  (void                      ) const;
+          void           setStatCollector  (StatCollector *pStat      );
+
+          bool           getManageStatReset(void                     ) const;
+          void           setManageStatReset(bool          value      );
+
+          Material      *getGlobalOverride (void                      ) const;
+          void           setGlobalOverride (Material      *pMat       );
 
     /*! \}                                                                 */
     /*---------------------------------------------------------------------*/
     /*! \name                   Statistics                                 */
     /*! \{                                                                 */
-
-    StatCollector *getStatCollector  (void                ) const;
-    void           setStatCollector  (StatCollector *pStat);
-
-    bool           getResetStatistics(void                ) const;
-    void           setResetStatistics(bool           value);
 
     /*! \}                                                                 */
     /*---------------------------------------------------------------------*/
@@ -287,6 +287,7 @@ class OSG_SYSTEM_DLLMAPPING RenderActionBase : public RenderActionBaseParent
     Material           *_pGlobalOverride;
 
     StatCollector      *_pStatistics;
+    bool                _bManageStatReset;
     TraversalValidator *_pTravValidator;
 
     bool                _bResetStatistics;
