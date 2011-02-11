b8073525d20ad04dbacbe347129a69080a93465a
David Kabala <djkabala@gmail.com>
2011-02-11 10:12:43 -0600

    Added: Tree gui for the html documentation created when the documentaion is created separately for each library.
    The html tree is a html/javascript solution.  The tree separates the
    libraries by Core, and Contrib specifications.  The code for the tree
    structure is configured by the cmake build configuration.

--- CMake/BuildFunctions.cmake
+++ CMake/BuildFunctions.cmake
@@ -2116,23 +2116,6 @@ FUNCTION(OSG_SETUP_SEPARATE_LIBS_DOXYDOC)
 
     INCLUDE(${${PROJECT_NAME}_BUILD_FILE})
 
-
-
-#    FILE(APPEND ${OSG_${PROJECT_NAME}_DOXY_CONFIGURATION_FILE}
-#        "#############################################################################\n")
-
-#    IF(${PROJECT_NAME}_DOXY_EXTRA_INC)
-#        FILE(APPEND ${OSG_${PROJECT_NAME}_DOXY_CONFIGURATION_FILE}
-#            "# doc input files for ${PROJECT_NAME}\n\n")
-
-#        FOREACH(DOXYFILE ${${PROJECT_NAME}_DOXY_EXTRA_INC})
-#            FILE(APPEND ${OSG_${PROJECT_NAME}_DOXY_CONFIGURATION_FILE}
-#                "INPUT += ${DOXYFILE}\n")
-#        ENDFOREACH()
-
-#        FILE(APPEND ${OSG_${PROJECT_NAME}_DOXY_CONFIGURATION_FILE} "\n")
-#    ENDIF()
-
     FILE(APPEND ${OSG_${PROJECT_NAME}_DOXY_CONFIGURATION_FILE}
         "# source code input files for ${PROJECT_NAME}\n\n")
 
@@ -2142,6 +2125,22 @@ FUNCTION(OSG_SETUP_SEPARATE_LIBS_DOXYDOC)
     ENDFOREACH()
 
     FILE(APPEND ${OSG_${PROJECT_NAME}_DOXY_CONFIGURATION_FILE} "\n")
+
+    #Tree Code
+    STRING(REGEX MATCH OSGContrib*|OSGTest* _OSG_IS_CONTRIBLIB ${PROJECT_NAME})
+
+    STRING(REPLACE "OSG" "" LIBTREENAME "${PROJECT_NAME}")
+    STRING(REPLACE "Contrib" "" LIBTREENAME "${LIBTREENAME}")
+
+    IF(_OSG_IS_CONTRIBLIB)
+        FILE(APPEND "${OSG_DOXY_DOC_TREE_SCRIPT}"
+            "      insDoc(ContribNode, gLnk(\"R\", \"${LIBTREENAME}\", \"${OSG_DOXY_BASELIB_DOC_HTML_INDEX}/${PROJECT_NAME}/html/index.html\"))
+")
+    ELSE()
+        FILE(APPEND "${OSG_DOXY_DOC_TREE_SCRIPT}"
+            "      insDoc(CoreNode, gLnk(\"R\", \"${LIBTREENAME}\", \"${OSG_DOXY_BASELIB_DOC_HTML_INDEX}/${PROJECT_NAME}/html/index.html\"))
+")
+    ENDIF()
 
 ENDFUNCTION(OSG_SETUP_SEPARATE_LIBS_DOXYDOC)
 
@@ -2462,8 +2461,8 @@ ENDMACRO(OSG_CHECK_INSTALL)
 ##########################################################################
 # Add Tutorial builds as tests
 #
-MACRO(OSG_ADD_TUTORIAL_TESTS TUTORIAL_SOURCE_DIR)
-    IF(NOT ${OSG_CMAKE_PASS} STREQUAL "OSGSETUPUNITTEST")
+MACRO(OSG_ADD_TUTORIAL_TESTS TUTORIALS_NAME TUTORIAL_SOURCE_DIR)
+    IF(NOT OSGBUILD_UNITTESTS)
         RETURN()
     ENDIF()
 
@@ -2481,7 +2480,7 @@ MACRO(OSG_ADD_TUTORIAL_TESTS TUTORIAL_SOURCE_DIR)
         SET(TUTORIAL_NAME ${CMAKE_MATCH_1})
 
         #Add a test to see if the tutorial exists
-        ADD_TEST(NAME "${PROJECT_NAME}-${TUTORIAL_NAME}"
+        ADD_TEST(NAME "${TUTORIALS_NAME}-${TUTORIAL_NAME}"
                  COMMAND test -e ${TUTORIAL_EXE_PATH})
     ENDFOREACH()
 ENDMACRO()
